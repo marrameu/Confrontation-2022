bacb6358441a04554dce9ba6082b4b69f1bfd339
Aleix Ràfols Bayot <41304995+marrameu@users.noreply.github.com>
2022-08-10 16:56:26 +0200

    Rolling stones

--- src/Troops/Roll.gd
+++ src/Troops/Roll.gd
@@ -10,19 +10,26 @@ func enter():
 	roll_direction.y = 0
 	roll_direction = roll_direction.normalized()
 	
-	velocity += roll_direction * 20
+	if !roll_direction: # potser q no hagi rpemut cal teclat, es a dir, no canvia de direcció
+		roll_direction = aim.z
+	# animació
+	owner.get_node("Model").rotation.y = Vector2(roll_direction.z, roll_direction.x).angle()
+	velocity = roll_direction * 25 # impuls
 	
-	$Timer.start()
+	owner.get_node("AnimationPlayer").play("Roll")
 
 
 func update(delta):
+	# La MAX_SPEED hauria de ser una corba 0.5-1-0 (al final q esperi uns segons per aixecar-se)
+	# nse de moment així a miem fa el fet
 	move(delta, MAX_SPEED, roll_direction)
 
 
-func _on_Timer_timeout(): # en lloc de timer -> animació
-	emit_signal("finished", "walk")
+func _on_animation_finished(anim_name):
+	if anim_name == "Roll":
+		emit_signal("finished", "walk")
 
 
 func exit():
-	# velocity = 0?
-	pass
+	velocity = Vector3(0, velocity.y, 0) # em penso q pot causar errors
+	owner.get_node("Model").rotation.y = 0
