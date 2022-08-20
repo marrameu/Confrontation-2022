57ed8b29ce5f1dd9ad0c5670e0947e2153f71154
Aleix Ràfols Bayot <41304995+marrameu@users.noreply.github.com>
2022-08-20 18:12:18 +0200

    Better damageindicator

--- src/HUD/DamageIndicator.gd
+++ src/HUD/DamageIndicator.gd
@@ -4,9 +4,16 @@ var attacker : Spatial
 var myself : Spatial
 
 
+func _ready():
+	restart_timer()
+
+
 func _on_Timer_timeout():
 	queue_free()
 
 
 func restart_timer():
+	var tween := get_tree().create_tween()
+	modulate = Color.white
+	tween.tween_property(self, "modulate", Color("00ffffff"), $Timer.wait_time)
 	$Timer.start()
